250810add819a7849c7fb0fb0854f19fc9417e7e
archita-b <archita.bhattacharyya91@gmail.com>
2024-12-19 09:39:19 +0530

    update schema

--- backend/model/schema/schema.sql
+++ backend/model/schema/schema.sql
@@ -1,13 +1,22 @@
 CREATE TYPE gender_enum AS ENUM ('male', 'female', 'other');
+CREATE TYPE appointment_status AS ENUM ('scheduled', 'cancelled', 'rescheduled');
 
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
+CREATE TABLE patients (
+    patient_id SERIAL PRIMARY KEY,
     full_name VARCHAR(100) NOT NULL,
-    username VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(200) NOT NULL,
     gender gender_enum NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    dob DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
 );
+
+-- only for form login
+-- CREATE TABLE auth (
+--     user_id INT PRIMARY KEY REFERENCES users(user_id) ON DELETE CASCADE,
+--     username VARCHAR(100) UNIQUE NOT NULL,
+--     password VARCHAR(100) NOT NULL
+-- );
 
 CREATE TABLE doctors (
     doctor_id SERIAL PRIMARY KEY,
@@ -15,7 +24,9 @@ CREATE TABLE doctors (
     speciality VARCHAR(100) NOT NULL,
     description VARCHAR(255),
     fees INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
 );
 
 CREATE TABLE time_slots (
@@ -23,16 +34,16 @@ CREATE TABLE time_slots (
     doctor_id INT REFERENCES doctors(doctor_id),
     slot_date DATE NOT NULL,
     start_time TIME NOT NULL,
-    end_time TIME NOT NULL,
-    is_booked BOOLEAN DEFAULT FALSE,
-    UNIQUE (doctor_id, slot_date, start_time, end_time)
+    duration INT NOT NULL CHECK (duration > 0)
 );
 
 CREATE TABLE appointments (
     appointment_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(user_id),
+    patient_id INT REFERENCES patients(patient_id),
     doctor_id INT REFERENCES doctors(doctor_id),
     appointment_slot INT REFERENCES time_slots(slot_id),
-    status VARCHAR(50) DEFAULT 'scheduled',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    status appointment_status DEFAULT 'scheduled',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 );
+
