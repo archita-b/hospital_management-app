17d81fce49db13c072b1960e56e15080e084b5da
mohamedaaqib3005 <mohamedaaqib3005@gmail.com>
2025-06-03 11:06:10 +0530

    format server.js

--- backend/server.js
+++ backend/server.js
@@ -12,30 +12,29 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors(
-  {
-    origin:"http://127.0.0.1:5500",
-    methods:"GET,POST,PUT,DELETE",
-    credentials:true
-  }
-))
+app.use(
+	cors({
+		origin: "http://127.0.0.1:5500",
+		methods: "GET,POST,PUT,DELETE",
+		credentials: true,
+	}),
+);
 
 app.use("/api", authRouter);
 app.use("/api", appointmentRouter);
 app.use("/api", doctorRouter);
 
 app.use((err, req, res, next) => {
-  const errorStatus = err.status || 500;
-  const errorMessage = err.message || "Internal Server Error";
+	const errorStatus = err.status || 500;
+	const errorMessage = err.message || "Internal Server Error";
 
-  return res.status(errorStatus).json({
-    success: false,
-    status: errorStatus,
-    message: errorMessage,
-  });
+	return res.status(errorStatus).json({
+		success: false,
+		status: errorStatus,
+		message: errorMessage,
+	});
 });
 
-
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+	console.log(`Server is running at http://localhost:${port}`);
 });
